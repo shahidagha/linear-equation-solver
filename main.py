6c21d6b0f425400f5ce930cefe37a15cbd6d199b
shahid <shahidagha@hotmail.com>
2026-03-08 04:03:49 +0530

    Added graphical table generator

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
-from step_engine.step_builder import StepBuilder
+from solver.graphical_solver import GraphicalSolver
 
-steps = StepBuilder()
+solver = GraphicalSolver()
 
-steps.add("Equation (1)", "2x + 3y = 4")
-steps.add("Equation (2)", "x + y = 1")
-steps.add("Multiply equation (2) by 2", "2x + 2y = 2")
-steps.add("Subtract equations", "y = 2")
-steps.add("Substitute y = 2", "x = -1")
+# equation x + y = 5
+points = solver.generate_points(1,1,5)
 
-print("SHORT MODE")
-steps.show("short")
-
-print("\nNORMAL MODE")
-steps.show("normal")
+solver.generate_table(points)
--- solver/graphical_solver.py
+++ solver/graphical_solver.py
@@ -7,6 +7,7 @@ class GraphicalSolver:
         for x in range(-8, 9):
 
             if b != 0:
+
                 y = (c - a*x) / b
 
                 if y.is_integer():
@@ -14,9 +15,29 @@ class GraphicalSolver:
                     y = int(y)
 
                     if -8 <= y <= 8:
-                        points.append((x, y))
+
+                        points.append((x,y))
 
             if len(points) == 3:
                 break
 
         return points
+
+
+    def generate_table(self, points):
+
+        x_values = []
+        y_values = []
+        pair_values = []
+
+        for p in points:
+
+            x_values.append(p[0])
+            y_values.append(p[1])
+            pair_values.append(f"({p[0]},{p[1]})")
+
+        print("\nTable of Values")
+
+        print("x :", x_values)
+        print("y :", y_values)
+        print("(x,y):", pair_values)

710b79f1d6884e3d969c2c58f5647b10348b6bd3
shahid <shahidagha@hotmail.com>
2026-03-08 02:59:58 +0530

    SymPy installed and first equation solver working

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Linear Equation Solver Project Started")
+from sympy import symbols, Eq, solve
+
+# Define variables
+x, y = symbols('x y')
+
+# Define equations
+eq1 = Eq(2*x + 3*y, 4)
+eq2 = Eq(x + y, 1)
+
+# Solve system
+solution = solve((eq1, eq2), (x, y))
+
+print("Solution:", solution)

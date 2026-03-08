f47e5b0e575a4f13cd9283a7b544b87a117ac947
shahid <shahidagha@hotmail.com>
2026-03-08 17:58:11 +0530

    Added equationsystem class

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 from math_engine.fraction_surd import FractionSurd
 from math_engine.equation import Equation
+from math_engine.system import EquationSystem
 
 
-a = FractionSurd(3,2,1,1,1)     # 3√2
-b = FractionSurd(5,1,1,3,-1)    # -5/√3
-c = FractionSurd(7,1,1,1,1)     # 7
+# Equation 1
+a1 = FractionSurd(3,2,1,1,1)
+b1 = FractionSurd(5,1,1,3,-1)
+c1 = FractionSurd(7,1,1,1,1)
+
+eq1 = Equation(a1,b1,c1)
 
 
-eq = Equation(a,b,c)
+# Equation 2
+a2 = FractionSurd(2,1,1,1,1)
+b2 = FractionSurd(1,1,1,1,1)
+c2 = FractionSurd(4,1,1,1,1)
 
-print(eq)
+eq2 = Equation(a2,b2,c2)
+
+
+system = EquationSystem(eq1,eq2)
+
+print(system)
---
+++ math_engine/system.py
@@ -0,0 +1,17 @@
+from math_engine.equation import Equation
+
+
+class EquationSystem:
+
+    def __init__(self, eq1: Equation, eq2: Equation):
+        self.eq1 = eq1
+        self.eq2 = eq2
+
+    def __str__(self):
+
+        return f"""
+System of Equations
+
+{self.eq1}
+{self.eq2}
+"""

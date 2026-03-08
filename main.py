83e97201790079f39dc7772e28c6f0aa97f6230a
shahid <shahidagha@hotmail.com>
2026-03-08 17:55:15 +0530

    Added equation class

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from math_engine.fraction_surd import FractionSurd
+from math_engine.equation import Equation
 
 
 a = FractionSurd(3,2,1,1,1)     # 3√2
 b = FractionSurd(5,1,1,3,-1)    # -5/√3
+c = FractionSurd(7,1,1,1,1)     # 7
 
-print("a =", a)
-print("b =", b)
 
-print("a + b =", a.add(b))
-print("a * b =", a.multiply(b))
+eq = Equation(a,b,c)
+
+print(eq)
---
+++ math_engine/equation.py
@@ -0,0 +1,26 @@
+import sympy as sp
+from math_engine.fraction_surd import FractionSurd
+
+
+class Equation:
+
+    def __init__(self, a: FractionSurd, b: FractionSurd, c: FractionSurd, var1="x", var2="y"):
+        self.a = a
+        self.b = b
+        self.c = c
+        self.var1 = var1
+        self.var2 = var2
+
+    def sympy_equation(self):
+
+        x = sp.Symbol(self.var1)
+        y = sp.Symbol(self.var2)
+
+        lhs = self.a.to_sympy() * x + self.b.to_sympy() * y
+        rhs = self.c.to_sympy()
+
+        return sp.Eq(lhs, rhs)
+
+    def __str__(self):
+
+        return str(self.sympy_equation())

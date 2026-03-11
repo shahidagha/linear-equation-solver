99e6c0e9be95c157a8574af7a7f041b5533a29e2
Shahid Agha <shahidagha@hotmail.com>
2026-03-12 01:34:06 +0530

    Fix substitution solver to use symbolic coefficients

--- backend/solver/substitution_solver.py
+++ backend/solver/substitution_solver.py
@@ -1,24 +1,25 @@
-from sympy import symbols, Eq, solve
+import sympy as sp
+
 
 class SubstitutionSolver:
 
     def solve(self, system):
 
-        x, y = symbols('x y')
+        x = sp.Symbol(system.eq1.var1)
+        y = sp.Symbol(system.eq1.var2)
 
-        eq1 = Eq(system.eq1.a*x + system.eq1.b*y, system.eq1.c)
-        eq2 = Eq(system.eq2.a*x + system.eq2.b*y, system.eq2.c)
+        eq1 = sp.Eq(
+            system.eq1.a.to_sympy() * x + system.eq1.b.to_sympy() * y,
+            system.eq1.c.to_sympy(),
+        )
+        eq2 = sp.Eq(
+            system.eq2.a.to_sympy() * x + system.eq2.b.to_sympy() * y,
+            system.eq2.c.to_sympy(),
+        )
 
-        # Solve eq1 for x
-        x_expr = solve(eq1, x)[0]
-
-        # Substitute into eq2
+        x_expr = sp.solve(eq1, x)[0]
         eq_sub = eq2.subs(x, x_expr)
-
-        # Solve for y
-        y_value = solve(eq_sub, y)[0]
-
-        # Substitute back for x
+        y_value = sp.solve(eq_sub, y)[0]
         x_value = x_expr.subs(y, y_value)
 
-        return {x: x_value, y: y_value}
+        return {x: sp.simplify(x_value), y: sp.simplify(y_value)}
